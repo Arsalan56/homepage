d4be9dce297ef2827a57bd0a16235657646044c8
Arsalan <118858748+Arsalan56@users.noreply.github.com>
2024-04-29 08:21:44 -0400

    Create diagonal background and style some heading

--- src/index.js
+++ src/index.js
@@ -1,1 +1,8 @@
 import './style.css';
+import './images/profile-s.jpeg';
+import './images/profile-m.jpeg';
+import './images/profile-l.jpeg';
+import './images/img-prev.jpg';
+import './images/github.png';
+import './images/linkedin.png';
+import './images/x-icon.png';
