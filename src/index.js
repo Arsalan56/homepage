99a6d0c7238555a0fe816f38705119566c927ca4
Arsalan <118858748+Arsalan56@users.noreply.github.com>
2024-05-09 12:31:15 -0400

    Create some styling for mobile devices

--- src/index.js
+++ src/index.js
@@ -2,7 +2,6 @@ import './style.css';
 import './images/profile-s.jpeg';
 import './images/profile-m.jpeg';
 import './images/profile-l.jpeg';
-import './images/img-prev.jpg';
 import './images/github.png';
 import './images/linkedin.png';
 import './images/x-icon.png';
